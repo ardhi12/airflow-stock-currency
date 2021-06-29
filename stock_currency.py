b7286adacf58db571a27b7827d6c4f3340033701
ardhi <ardhiwahyudhi12@gmail.com>
2021-06-29 18:59:26 +0700

    add trasnform function

--- stock_currency.py
+++ stock_currency.py
@@ -1,7 +1,11 @@
 import yfinance as yf
 import requests
+from time import time
 
-def rupiah_format(value):        
+def rupiah_format(value):
+    """
+    This function is used to change format IDR
+    """        
     reverse = str(value)[::-1]
     temp_reverse_value = ""
 
@@ -14,24 +18,14 @@ def rupiah_format(value):
     result = temp_reverse_value[::-1]
     return result
 
-def get_stock_detail():
-    amzn = yf.Ticker("AMZN")
-    detail = {
-        "name": amzn.info["longName"],
-        "symbol": amzn.info["symbol"],
-        "industry": amzn.info["industry"],
-        "previousClose" : amzn.info["previousClose"]
-    }
-    return detail
-
 def latest_usd_idr():
     """
     This function is used to get the latest USD currency
     """
     try:        
-        response = requests.get('https://v6.exchangerate-api.com/v6/ac432da0abe6dd87ea30b9df/latest/USD', timeout=5)
+        response = requests.get("https://v6.exchangerate-api.com/v6/ac432da0abe6dd87ea30b9df/latest/USD", timeout=5)
         response.raise_for_status()    
-        return response.json()['conversion_rates']['IDR']        
+        return response.json()["conversion_rates"]["IDR"]        
     except requests.exceptions.HTTPError as errh:
         return errh
     except requests.exceptions.ConnectionError as errc:
@@ -41,6 +35,40 @@ def latest_usd_idr():
     except requests.exceptions.RequestException as err:
         return err
 
+def get_stock_detail():
+    """
+    This function is used to get Amazon stock detail
+    """
+    try:
+        amzn = yf.Ticker("AMZN")
+        detail = {
+            "name": amzn.info["longName"],
+            "symbol": amzn.info["symbol"],
+            "industry": amzn.info["industry"],
+            "previousClose" : amzn.info["previousClose"]
+        }
+        print(detail)
+    except Exception as e:
+        print(e)        
+    return detail
 
+def transform(ti):
+    """
+    This function is used to get the latest USD currency
+    """
+    # xcom_pull is used to get data from airflow database
+    datas = ti.xcom_pull(task_ids =[
+        "get_stock_detail",
+        "latest_usd_idr"
+    ])
+    millis = round(time() * 1000)
+    usd = round(datas[0]["previousClose"])                  
+    idr = usd * round(datas[1])
+    idr_format = rupiah_format(idr)
+    convert = {
+        "timestamp": millis,
+        "latest_price_usd": f"${usd}",
+        "latest_price_idr": f"Rp. {idr_format}"
+    }    
+    return convert
 
-rupiah_format(20000)
